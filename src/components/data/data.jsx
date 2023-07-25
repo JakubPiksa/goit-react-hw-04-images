e07054096551147cd7996996f4d69b1485ced35e
Jakub Piksa <kuba.piksa@wp.pl>
2023-07-25 16:19:56 +0200

    Update data.jsx

--- src/components/data/data.jsx
+++ src/components/data/data.jsx
@@ -1,28 +1,27 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import css from './data.module.css';
 
-export const Statistics = ({ title, stats }) => {
+const Statistics = ({ title, stats }) => {
   const getRandomColor = () => {
-    return `#${Math.floor(Math.random() * 16777215)
-      .toString(16)
-      .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
   };
-    return (
+
+  return (
     <section className={css.statistics}>
-        {title && <h2 className={css.title}>{title}</h2>}
-        <h2 className={css.text}> UPLOAD STATS</h2>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <h2 className={css.text}>UPLOAD STATS</h2>
       <ul className={css.stat_list}>
-        {stats.map((stat, index) => (
+        {stats.map(({ label, percentage }, index) => (
           <li key={index} className={css.item} style={{ backgroundColor: getRandomColor() }}>
-            <span className={css.label}>{stat.label}</span>
-            <span className={css.percentage}>{stat.percentage}%</span>
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}%</span>
           </li>
         ))}
       </ul>
     </section>
   );
 };
-
 
 Statistics.propTypes = {
   title: PropTypes.string,
@@ -34,4 +33,4 @@ Statistics.propTypes = {
   ).isRequired,
 };
 
-export default Statistics
+export default Statistics;
